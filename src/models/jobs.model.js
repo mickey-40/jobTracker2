7abc216783258b33f22a080b9e0ecfa98121faba
mickey-40 <mickeyarnold53@yahoo.com>
2024-11-14 20:13:37 -0600

    added sample data

--- src/models/jobs.model.js
+++ src/models/jobs.model.js
@@ -1,3 +1,41 @@
-const jobs = [];
+//temp data
+const jobs = [
+  {
+    jobTitle: "Software Developer",
+    dateApplied: "1/14/2024",
+    skillsInJobDes: "Javascript, Node.js, React.js",
+    calledBack: false,
+    contactsOnLinkedin: "Mary and Tom",
+    additionalEffort: "Contacted Bob hiring manager on linkedin and waiting on reply"
+
+  },
+  {
+    jobTitle: "Software Developer",
+    dateApplied: "1/14/2024",
+    skillsInJobDes: "Javascript, Node.js, React.js",
+    calledBack: false,
+    contactsOnLinkedin: "Mary and Tom",
+    additionalEffort: "Contacted Bob hiring manager on linkedin and waiting on reply"
+
+  },
+  {
+    jobTitle: "Software Engineer",
+    dateApplied: "1/16/2024",
+    skillsInJobDes: "Node.js, React.js, Mongodb",
+    calledBack: false,
+    contactsOnLinkedin: "Kyle Tomson",
+    additionalEffort: "Contacted Harry hiring manager on linkedin and waiting on reply"
+
+  },
+  {
+    jobTitle: "Backend Developer",
+    dateApplied: "1/18/2024",
+    skillsInJobDes: "Node.js, SQL, Mongodb",
+    calledBack: true,
+    contactsOnLinkedin: "Nick",
+    additionalEffort: "Contacted Tommy hiring manager on linkedin and waiting on reply"
+
+  },
+];
 
 module.exports = jobs;
